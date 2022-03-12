3e4ede499ed041f7ef8259214a7fa69922eb7c5e
Dragon302001 <96678471+Dragon302001@users.noreply.github.com>
2022-03-12 21:42:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "sakshi" && b=="1234")
+  	{
+  	    window.location.href="Birthday.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
